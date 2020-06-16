bcb7dc62ce08e4a9f0bffd5760a7be48eb35e2dc
plkujaw <jawor.jakub@gmail.com>
2020-06-16 11:13:16 +0100

    initial setup, sanity check and install browserify

---
+++ bundle.js
@@ -0,0 +1,42 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+const Phrase = require("plkujaw-palindrome");
+
+alert(new Phrase("Madam, I'm Adam.").palindrome());
+
+},{"plkujaw-palindrome":2}],2:[function(require,module,exports){
+module.exports = Phrase;
+
+// Adds 'reverse' to all strings.
+
+String.prototype.reverse = function reverse() {
+  return Array.from(this).reverse().join("");
+}
+
+//Defines a Phrase object (class).
+
+function Phrase(content) {
+  this.content = content;
+
+  this.letters = function letters() {
+    return (this.content.match(/[a-z]/gi) || []).join("");
+    // we use || [] because match returns null if there is no match and throws an error
+  }
+
+  this.processor = function() {
+    return this.letters().toLowerCase();
+  }
+
+  // Returns content processed for palindrome testing,
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+
+  // Returns true for phrase being a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+    // returns result of this comparison
+  }
+
+}
+
+},{}]},{},[1]);
---
+++ main.js
@@ -0,0 +1,3 @@
+const Phrase = require("plkujaw-palindrome");
+
+alert(new Phrase("Madam, I'm Adam.").palindrome());
