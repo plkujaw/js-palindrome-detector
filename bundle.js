22843821f3dd80ee42f9852ed4ab07cf1ccd7b28
plkujaw <jawor.jakub@gmail.com>
2020-06-16 11:23:30 +0100

    added simplest version of detector

--- bundle.js
+++ bundle.js
@@ -1,7 +1,13 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const Phrase = require("plkujaw-palindrome");
 
-alert(new Phrase("Madam, I'm Adam.").palindrome());
+let string = prompt("Please enter a string to test:");
+let phrase = new Phrase(string);
+  if (phrase.palindrome()) {
+    alert(`"${phrase.content}" is a palindrome.`);
+  } else {
+    alert(`"${phrase.content}" is not a palindrome.`);
+  }
 
 },{"plkujaw-palindrome":2}],2:[function(require,module,exports){
 module.exports = Phrase;
--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const Phrase = require("plkujaw-palindrome");
 
-alert(new Phrase("Madam, I'm Adam.").palindrome());
+let string = prompt("Please enter a string to test:");
+let phrase = new Phrase(string);
+
+if (phrase.palindrome()) {
+  alert(`"${phrase.content}" is a palindrome.`);
+} else {
+  alert(`"${phrase.content}" is not a palindrome.`);
+}
